6da27521c81040240f6380ca134295fd57f9216f
Jason Eu <morty.yu@yahoo.com>
2019-05-17 15:05:36 +0800

    ci_transifex test

--- .ci/transifex_upload.sh
+++ .ci/transifex_upload.sh
@@ -8,9 +8,9 @@ pip install virtualenv
 virtualenv ~/env
 source ~/env/bin/activate
 pip install transifex-client
-pip install .[docs,testing]
+# pip install .[docs,testing]
 # make gettext
-sphinx-build -b gettext docs/source locale
-rm locale/apidoc.pot
+# sphinx-build -b gettext docs/source locale
+# rm locale/apidoc.pot
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_PASSWORD"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
 tx push -s
