f8afefa6788eb0a35ae25ced59618a66efda7fb1
Jason Eu <morty.yu@yahoo.com>
2019-05-17 14:03:58 +0800

    ci_transifex test

---
+++ .ci/transifex_upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Be verbose, and stop with error as soon there's one
+set -ev
+
+pip install virtualenv
+virtualenv ~/env
+source ~/env/bin/activate
+pip install transifex-client
+pip install .[docs,testing]
+cd docs
+make gettext
+sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_PASSWORD"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
+tx push -s
